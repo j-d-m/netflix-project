fb8aa1da0e7e1942f3ce515394f48653d3be5f12
Joshua Moir <j.moir.nz@gmail.com>
2021-10-29 09:54:42 +0200

    created a front end basic branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">NetFlix</div>;
+  return <div className="App">NetFlix !!</div>;
 }
 
 export default App;
